d49218471a2058a9c76af8aa213eb08889384df3
chandan prajapati <162724475+chandanprajapa@users.noreply.github.com>
2025-05-17 19:52:05 +0530

    Update db.js

--- food-delivery-app/food-delivery-app/backend/config/db.js
+++ food-delivery-app/food-delivery-app/backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
 
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://MCAProject:Test%401234@cluster0.zxegi.mongodb.net/chandan?retryWrites=true&w=majority').then(()=>console.log("DataBase Connect"));
+    await mongoose.connect('mongo_uri').then(()=>console.log("DataBase Connect"));
 }
